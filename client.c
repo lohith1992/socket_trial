9ee4ca3a7526612b7509c6e1f20241df81543144
Bs, Lohith <lohith.bs@intel.com>
2016-09-25 14:33:01 +0530

    sep 2:35pm

--- client.c
+++ client.c
@@ -16,7 +16,7 @@ int main(int argc, char *argv[])
     char recvBuff[1024];
     struct sockaddr_in serv_addr; 
 
-	printf("Hi lohith  my first second change github/n");
+	printf("Hi old github trial/n");
     if(argc != 2)
     {
         printf("\n Usage: %s <ip of server> \n",argv[0]);
